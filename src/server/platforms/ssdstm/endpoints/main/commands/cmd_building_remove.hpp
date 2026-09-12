8db77011b3a30c6717ff29c2a67598a07937100e
alice <58637860+alicealys@users.noreply.github.com>
2026-09-12 03:45:31 +0200

    fix build

--- src/server/platforms/ssdstm/endpoints/main/commands/cmd_building_remove.hpp
+++ src/server/platforms/ssdstm/endpoints/main/commands/cmd_building_remove.hpp
@@ -6,6 +6,6 @@ namespace emulator::ssd
 	class cmd_building_remove final : public command_handler
 	{
 		nlohmann::json execute(nlohmann::json& data, const std::optional<database::users::user>& user) override;
-		std::uint32_t flags();
+		std::uint32_t flags() override;
 	};
 }
