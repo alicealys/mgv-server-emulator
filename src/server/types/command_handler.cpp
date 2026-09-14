efd05507ff9f2b85afc546ef9e16f496cf6bc656
alice <58637860+alicealys@users.noreply.github.com>
2026-09-14 04:31:19 +0200

    fix

--- src/server/types/command_handler.cpp
+++ src/server/types/command_handler.cpp
@@ -29,10 +29,10 @@ namespace emulator
 		glz::json info;
 
 		info["npid"]["handler"]["data"] = "";
-		info["npid"]["handler"]["dummy"] = {0, 0, 0};
+		info["npid"]["handler"]["dummy"] = glz::json::array_t{0, 0, 0};
 		info["npid"]["handler"]["term"] = 0;
-		info["npid"]["opt"] = {0, 0, 0, 0, 0, 0, 0, 0};
-		info["npid"]["reserved"] = {0, 0, 0, 0, 0, 0, 0, 0};
+		info["npid"]["opt"] = glz::json::array_t{0, 0, 0, 0, 0, 0, 0, 0};
+		info["npid"]["reserved"] = glz::json::array_t{0, 0, 0, 0, 0, 0, 0, 0};
 		info["player_id"] = player_id;
 		info["player_name"] = account_id == 0 ? "NotImplement" : std::format("{}_player01", account_id);
 		info["ugc"] = player_id != 0 ? 1 : 0;
